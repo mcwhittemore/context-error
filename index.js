29bb9729d6b6a5ccd11296b325c8bfcc99c7466e
Matthew Chase Whittemore <matthew@socialtables.com>
2013-11-07 01:23:41 -0500

    seeding version 0.0.0

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = function(message, stackNumber) {
+    var err = new Error(message);
+    if (err.stack) {
+        var parts = err.stack.split("\n");
+        if (parts[stackNumber]) {
+            var addon = parts[stackNumber].split("(")[1].split(")")[0];
+            return new Error(err.message + " from " + addon);
+        }
+    }
+    return err;
+}
---
+++ test/test.js
@@ -0,0 +1,47 @@
+var should = require("should");
+var ContextError = require("../");
+
+describe("Refernce the right line", function() {
+
+    var one = function(callback) {
+        callback();
+    }
+
+    var two = function(callback) {
+        one(callback);
+    }
+
+    var three = function(callback) {
+        two(callback);
+    }
+
+    it("Should mention function three", function() {
+        three(function() {
+            try {
+                throw ContextError("Line 15", 5);
+            } catch (err) {
+                err.message.split(":")[1].should.equal("15");
+            }
+        });
+    });
+
+    it("Should mention function two", function() {
+        two(function() {
+            try {
+                throw ContextError("Line 11", 4);
+            } catch (err) {
+                err.message.split(":")[1].should.equal("11");
+            }
+        });
+    });
+
+    it("Should mention function one", function() {
+        one(function() {
+            try {
+                throw ContextError("Line 7", 3);
+            } catch (err) {
+                err.message.split(":")[1].should.equal("7");
+            }
+        });
+    });
+});
